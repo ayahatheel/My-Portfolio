942f069511dc526897564bedfad35f3ebb01e9f2
Ayah Atheel <66213838+ayahatheel@users.noreply.github.com>
2024-03-23 14:31:29 +0300

    instagram

--- index.js
+++ index.js
@@ -22,20 +22,63 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //sroll
+
 window.addEventListener('scroll', function() {
   var mySkillsSection = document.getElementById('MYSKILLS');
+  var cardsContainer = document.querySelector('.cards');
+
   var scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-  if (scrollPosition > 200) {
+  var mySkillsPosition = mySkillsSection.offsetTop;
+  var cardsPosition = cardsContainer.offsetTop;
+
+  var buffer = 200; 
+
+  // MYSKILLS Section
+  if (scrollPosition + buffer > mySkillsPosition) {
       mySkillsSection.style.opacity = 1;
+      mySkillsSection.style.transform = 'translateY(0)';
       mySkillsSection.style.visibility = 'visible';
   } else {
       mySkillsSection.style.opacity = 0;
+      mySkillsSection.style.transform = 'translateY(20px)';
       mySkillsSection.style.visibility = 'hidden';
+  }
+
+
+  // Cards Section
+  if (scrollPosition + buffer > cardsPosition) {
+      cardsContainer.style.opacity = 1;
+      cardsContainer.style.transform = 'translateY(0)';
+      cardsContainer.style.visibility = 'visible';
+  } else {
+      cardsContainer.style.opacity = 0;
+      cardsContainer.style.transform = 'translateY(20px)';
+      cardsContainer.style.visibility = 'hidden';
   }
 });
 
 
-//sroll
+// linking 
+// function smoothScroll(targetSelector, duration) {
+//   const target = document.querySelector(targetSelector);
+//   const targetPosition = target.getBoundingClientRect().top;
+//   const startPosition = window.pageYOffset;
+//   const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
 
+//   const easeInOutQuad = (time, start, change, duration) => {
+//       time /= duration / 2;
+//       if (time < 1) return change / 2 * time * time + start;
+//       time--;
+//       return -change / 2 * (time * (time - 2) - 1) + start;
+//   };
 
+//   const animateScroll = currentTime => {
+//       const timeElapsed = currentTime - startTime;
+//       const run = easeInOutQuad(timeElapsed, startPosition, targetPosition, duration);
+//       window.scrollTo(0, run);
+//       if (timeElapsed < duration) requestAnimationFrame(animateScroll);
+//   };
+
+//   requestAnimationFrame(animateScroll);
+// }
